7088785b0bc5367daf33b4b26b85f351b438b1b3
aswinthaikkat <aswinthaikkat@.com>
2018-10-03 00:18:41 +0530

    Successfully Added RXJAVA2....Better prediction...Control and flow achieved....

--- app/src/main/java/com/example/user/retrofitandroid/Api.java
+++ app/src/main/java/com/example/user/retrofitandroid/Api.java
@@ -4,6 +4,7 @@ package com.example.user.retrofitandroid;
 
 import java.util.List;
 
+import io.reactivex.Observable;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
@@ -11,5 +12,8 @@ public interface Api {
 
     String BASE_URL = "https://simplifiedcoding.net/demos/";
     @GET("marvel")
-    Call<List<Marvel>> getMarvel();
+    Call <List<Marvel>> getMarvel();
+
+    @GET("marvel")
+    Observable <List<Marvel>> getMarvel2();
 }
--- app/src/main/java/com/example/user/retrofitandroid/MainActivity.java
+++ app/src/main/java/com/example/user/retrofitandroid/MainActivity.java
@@ -6,61 +6,123 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.SingleSource;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
+    List<String> Heronames = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView = findViewById(R.id.mListView);
-        getMarvel();
+        //getMarvel();
+        getMarvel2();
+
     }
 
 
+//    private void getMarvel() {
+//        Retrofit retrofit = new Retrofit.Builder()
+//                .baseUrl(Api.BASE_URL)
+//                .addConverterFactory(GsonConverterFactory.create()) //Here we are using the GsonConverterFactory to directly convert json data to object
+//                .build();
+//
+//        Api api = retrofit.create(Api.class);
+//
+//        Call<List<Marvel>> call = api.getMarvel();
+//
+//        call.enqueue(new Callback<List<Marvel>>() {
+//            @Override
+//            public void onResponse(Call<List<Marvel>> call, Response<List<Marvel>> response) {
+//                List<Marvel> heroList = response.body();
+//
+//                //Creating an String array for the ListView
+//                String[] heroes = new String[heroList.size()];
+//
+//                //looping through all the heroes and inserting the names inside the string array
+//                for (int i = 0; i < heroList.size(); i++) {
+//                    heroes[i] = heroList.get(i).getName();
+//                }
+//
+//
+//                //displaying the string array into listview
+//                listView.setAdapter(new ArrayAdapter<String>(getApplicationContext(), R.layout.list_black_text, R.id.list_content, heroes));
+//
+//            }
+//
+//            @Override
+//            public void onFailure(Call<List<Marvel>> call, Throwable t) {
+//                Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//    }
 
-    private void getMarvel() {
+    private void getMarvel2() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(Api.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create()) //Here we are using the GsonConverterFactory to directly convert json data to object
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())//Here we are using the GsonConverterFactory to directly convert json data to object
                 .build();
 
         Api api = retrofit.create(Api.class);
 
-        Call<List<Marvel>> call = api.getMarvel();
 
-        call.enqueue(new Callback<List<Marvel>>() {
+        api.getMarvel2().flatMap(new Function<List<Marvel>, ObservableSource<Marvel>>() {
             @Override
-            public void onResponse(Call<List<Marvel>> call, Response<List<Marvel>> response) {
-                List<Marvel> heroList = response.body();
+            public ObservableSource<Marvel> apply(List<Marvel> marvels) throws Exception {
+                return Observable.fromIterable(marvels);
+            }
+        }).flatMap(new Function<Marvel, ObservableSource<String>>() {
+            @Override
+            public ObservableSource<String> apply(Marvel marvel) throws Exception {
+                return Observable.just(marvel.getName());
+            }
+        }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
 
-                //Creating an String array for the ListView
-                String[] heroes = new String[heroList.size()];
+                Toast.makeText(getApplicationContext(), "Started loading Data", Toast.LENGTH_SHORT).show();
+            }
 
-                //looping through all the heroes and inserting the names inside the string array
-                for (int i = 0; i < heroList.size(); i++) {
-                    heroes[i] = heroList.get(i).getName();
-                }
-
-
-                //displaying the string array into listview
-                listView.setAdapter(new ArrayAdapter<String>(getApplicationContext(),R.layout.list_black_text, R.id.list_content, heroes));
+            @Override
+            public void onNext(String s) {
+                Heronames.add(s);
 
             }
 
             @Override
-            public void onFailure(Call<List<Marvel>> call, Throwable t) {
-                Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
+            public void onError(Throwable e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onComplete() {
+                Toast.makeText(getApplicationContext(), "Completed loading Data", Toast.LENGTH_SHORT).show();
+                listView.setAdapter(new ArrayAdapter<String>(getApplicationContext(), R.layout.list_black_text, R.id.list_content, Heronames));
+
             }
         });
+
+
     }
 }
